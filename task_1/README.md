6d951114a8d009af651717a5f41d55f88320e209
Ayllonbe <ayllonbenitez.aaron@gmail.com>
2020-07-13 18:54:40 +0200

    Conversion completed

---
+++ task_1/README.md
@@ -0,0 +1,11 @@
+# TASK 1 - Extract PLIP Contacts for any ligand bound to a protein chain from XML to JSON
+
+The xmlToJSON.py get a xml from Protein-Ligand Interaction Profiler (PLIP) database and provide a JSON file. 
+
+To run the script in the shell you can use the following command line:
+
+> ./xmlToJSON.py -x plipFile.xml
+
+For further information:
+
+> ./xmlToJSON.py -h
--- task_1/xmlToJSON.py
+++ task_1/xmlToJSON.py
@@ -1,12 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 2020
+
+@author: Aaron Ayllon Benitez
+@description: Hackathon ISMB 2020 - Team 3A - Task 1
+"""
+import sys, getopt
 import xml.etree.ElementTree as ET
 import json
 
 def returnNumeric(s):
-    try: 
-        
-        return int(s)
-    except ValueError:
-        return float(s)
+    if s !=None :
+        x = s.split(".")
+        if(x[0].isdigit() or  ("-" in x[0] and x[0].split("-")[1].isdigit())):
+            try: 
+            
+                return int(s)
+            except ValueError:
+                return float(s)
+        else:
+            return s
+    else:
+        return s
 
 def getJSON(r,export):
     r = json.dumps(r,indent=2)
@@ -14,36 +30,40 @@ def getJSON(r,export):
     #loaded_r['identifiers'] #Output 3.5
     #type(r) #Output str
     #type(loaded_r) #Output dict
-    f = open(export+".json", "w")
+    f = open(export, "w")
     f.write(r)
     f.close()
-def recursiveTags(elem,dic):
-    for e in list(elem):
-        if len(list(e))>0:
-            dic[e.tag] = {}
-            recursiveTags(e,dic[e.tag])
-        else:
-            if e.text!=None and len(e.items())>0:
-                dic[e.tag] = {}
-                
-                x = e.text.split(".")
-                if(x[0].isdigit() or  ("-" in x[0] and x[0].split("-")[1].isdigit())):
-                    dic[e.tag]["value"] = returnNumeric(e.text)
+def recursiveTags(elem):
+
+    if(list(elem)[0].tag in elem.tag):
+        dic = {}
+        for e in list(elem):
+            if len(list(e))>0:
+                dic[e.attrib["id"]] = recursiveTags(e)
+            else:
+                if e.text!=None and len(e.items())>1:
+                    dicItems = {}
+                    for t in e.items():
+                        if t[0] != "id":
+                            dicItems[t[0]] = returnNumeric(t[1])
+                    dicItems["value"] = returnNumeric(e.text)
+                    
+                    dic[e.attrib["id"]] = dicItems
                 else:
-                    dic[e.tag]["value"] = e.text
-                dic[e.tag]["atr"] = {}
-                for i in e.items():
-                    dic[e.tag]["atr"][i[0]] = i[1]
-            elif e.text!=None:
-                x = e.text.split(".")
-                if x[0].isdigit() or ("-" in x[0] and x[0].split("-")[1].isdigit()):
-                    dic[e.tag] = returnNumeric(e.text)
-                else:
-                     dic[e.tag] = e.text
-               
+                    dic[e.attrib["id"]] = returnNumeric(e.text)
+        return dic
+    else:
+        dic = {}
+        for e in list(elem):
+            if len(list(e))>0:
+                dic[e.tag] = recursiveTags(e)
+            else:
+                dic[e.tag] = returnNumeric(e.text)
+        return dic
 
 
-def parseXML(file_name):
+
+def parseXML(file_name,output):
    # Parse XML with ElementTree
    tree = ET.ElementTree(file=file_name)
    root = tree.getroot()
@@ -54,47 +74,46 @@ def parseXML(file_name):
    print("-" * 25)
    dicF={}
    dicF["bindingsites"] = {}
-   dicF["covlinkages"]  = {}
+   #dicF["covlinkages"]  = {}
    users =list(root)
    for user in users:
-      if user.tag == "bindingsite":
-          dic={}
-          user_children = list(user)
-          dicF["bindingsites"][user.items()[0][1]]={}
-          dicF["bindingsites"][user.items()[0][1]]["has_interactions"]=user.items()[1][1]
-          
-          
-          for user_child in user_children:
-             if user_child.tag == "identifiers": 
-                dic["identifiers"] = {} 
-                recursiveTags(list(user_child),dic["identifiers"])
-             if user_child.tag == "lig_properties":
-                 dic["lig_properties"] = {} 
-                 recursiveTags(list(user_child),dic["lig_properties"])
-             if user_child.tag == "interacting_chains":
-                 dic["interacting_chains"] = {} 
-                 recursiveTags(list(user_child),dic["interacting_chains"])
-             if user_child.tag == "bs_residues":
-                 dic["bs_residues"] = {} 
-                 recursiveTags(list(user_child),dic["bs_residues"])    
-             if user_child.tag == "interactions":
-                 dic["interactions"] = {} 
-                 recursiveTags(list(user_child),dic["interactions"])  
-             if user_child.tag == "mappings":
-                 dic["mappings"] = {} 
-                 recursiveTags(list(user_child),dic["mappings"])
-          dicF["bindingsites"][user.items()[0][1]]["information"]=dic
-      if user.tag == "covlinkages":
-          dicF["covlinkages"]={}
-          user_children = list(user)
-          
-          for user_child in user_children:
-              
-              dicF["covlinkages"][user_child.items()[0][1]]={}
-              recursiveTags(list(user_child),dicF["covlinkages"][user_child.items()[0][1]])
-          
+      if user.tag == "bindingsite" :
+          dicF["bindingsites"][user.attrib["id"]] = {}
+          dicF["bindingsites"][user.attrib["id"]]["has_interactions"] = returnNumeric(user.attrib["has_interactions"])
+          dicF["bindingsites"][user.attrib["id"]] = recursiveTags(user)
+         
+      elif user.tag == "covlinkages" and len(list(user))>0:
+           dicF[user.tag]= recursiveTags(user)
+      elif user.tag == "pdbid":
+          dicF[user.tag] = user.text
         
-   getJSON(dicF,file_name.split(".")[0])
+   getJSON(dicF,output)
+
+def main(argv):
+   xmlF = ''
+   jsonF = ''
+   try:
+     # opts, args = getopt.getopt(argv,"hx:j:",["xmlfile=","jsonfile="])
+     opts, args = getopt.getopt(argv,"hx:",["xmlfile="])
+   except getopt.GetoptError:
+      print('[error] xmlToJSON.py -x <xmlfile> [-j <jsonfile>]')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('xmlToJSON.py -x <xmlfile> -j <jsonfile>')
+         print("-" * 25)
+         print('-x --xmlfile:\tPILP xml file input')
+         print('-j --jsonfile:\t [OPT] json file output. Default: [xmlfileName.json]')
+         
+         
+         sys.exit()
+      elif opt in ("-x", "--xmlfile"):
+         xmlF = arg
+      elif opt in ("-j", "--jsonfile"):
+         jsonF = arg
+   if jsonF == "":
+        jsonF = xmlF.split(".")[0]+".json"
+   parseXML(xmlF,jsonF)
 
 if __name__ == "__main__":
-   parseXML("test_data/2hu4.xml")
+   main(sys.argv[1:])
